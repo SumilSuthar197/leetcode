444e199f76a33c0a6a52e010844fa1fe17b5b775
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2023-07-13 14:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (43.91%) - LeetSync

--- merge-strings-alternately/merge-strings-alternately.cpp
+++ merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int n=min(word1.length(),word2.length());
-        string ans;
-        for(int i=0;i<n;i++){
-            ans+=word1[i];
-            ans+=word2[i];
+        int p1=0,p2=0;
+        string ans="";
+        while(p1!=word1.size() || p2!=word2.size()){
+            if(p1!=word1.size()){
+                ans+=word1[p1];
+                p1++;
+            }
+            if(p2!=word2.size()){
+                ans+=word2[p2];
+                p2++;
+            }
         }
-        return ans+word1.substr(n)+word2.substr(n);
+        return ans;
     }
 };
