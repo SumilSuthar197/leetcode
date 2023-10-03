ccc28ea3b9fcaddf22bca2f59b70c7e5b4c386b4
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2023-10-03 12:22:17 +0530

    Time: 18 ms (39.40%) | Memory: 15.1 MB (13.03%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans="",temp="";
-        int i=0;
-        while(i!=s.length()){
-            if(s[i]==' '){
-                ans+=temp+" ";
-                temp="";
-            }
-            else{
-                temp=s[i]+temp;
-            }
-            i++;
+        // string ans="",temp="";
+        // int i=0;
+        // while(i!=s.length()){
+        //     if(s[i]==' '){
+        //         ans+=temp+" ";
+        //         temp="";
+        //     }
+        //     else{
+        //         temp=s[i]+temp;
+        //     }
+        //     i++;
+        // }
+        // ans+=temp;
+        // return ans;
+        stringstream ss(s);
+        string temp,ans;
+        while(ss>>temp){
+            reverse(temp.begin(),temp.end());
+            ans+= (temp+" ");
         }
-        ans+=temp;
-        return ans;
+        return ans.substr(0,ans.size()-1);
     }
 };
