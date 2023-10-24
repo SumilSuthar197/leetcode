b9360771e8265cfdf75ce35fc7383c21f784668a
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2023-10-24 17:19:22 +0530

    Time: 106 ms (64.36%) | Memory: 74 MB (42.35%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
+    int atMost(vector<int> nums,int k){
+        int l=0,r=0,ans=0;
+
+        for(;r<nums.size();r++){
+            k-=nums[r]%2;
+            for(;k<0;l++){
+                k+=nums[l]%2;
+            }
+            ans+=r-l+1;
+        }
+        return ans;
+    }
+
     int numberOfSubarrays(vector<int>& nums, int k) {
         for(int i=0;i<nums.size();i++) nums[i]%=2;
         // odd=1, even=0
         // Now question is find subarray where sum is k;
-        //Method: Prefix Sum + Hashing
-        unordered_map<int,int> mp;
-        int sum=0,c=0,l=0,r=0;
-        mp[0]=1;
-        for(;r<nums.size();r++){
-            sum+=nums[r];
-            if(mp.count(sum-k)){
-                c+=mp[sum-k];
-            }
-            mp[sum]++;
-        }
-        return c;
+        //Method 1: Prefix Sum + Hashing
+
+        // unordered_map<int,int> mp;
+        // int sum=0,c=0,l=0,r=0;
+        // mp[0]=1;
+        // for(;r<nums.size();r++){
+        //     sum+=nums[r];
+        //     if(mp.count(sum-k)){
+        //         c+=mp[sum-k];
+        //     }
+        //     mp[sum]++;
+        // }
+        // return c;
+
+        //Method 2:
+        return atMost(nums,k)-atMost(nums,k-1);
     }
 };
