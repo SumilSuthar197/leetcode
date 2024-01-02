a1aa63bc05b3dabb814a870110e53bc0b4958043
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2024-01-02 12:31:04 +0530

    Time: 22 ms (10.81%) | Memory: 29.9 MB (70.96%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-       unordered_map<int,int> mp;
-       int maxLength=0;
-       for(int i:nums){
-           mp[i]++;
-           maxLength=max(maxLength,mp[i]);
-       }
-       vector<vector<int>> ans(maxLength, vector<int>{});
-       for(auto i:mp){
-           int j=0;
-           while(i.second>0){
-               ans[j].push_back(i.first);
-               j++;
-               i.second--;
-           }
-       }
-       return ans;
+        vector<int> freq(nums.size()+1);
+        vector<vector<int>> ans;
+        for(int i:nums){
+            if(freq[i]>=ans.size()) ans.push_back({});
+            ans[freq[i]].push_back(i);
+            freq[i]++;
+        }
+        return ans;
+
+    // O(n^2):
+    //    unordered_map<int,int> mp;
+    //    int maxLength=0;
+    //    for(int i:nums){
+    //        mp[i]++;
+    //        maxLength=max(maxLength,mp[i]);
+    //    }
+    //    vector<vector<int>> ans(maxLength, vector<int>{});
+    //    for(auto i:mp){
+    //        int j=0;
+    //        while(i.second>0){
+    //            ans[j].push_back(i.first);
+    //            j++;
+    //            i.second--;
+    //        }
+    //    }
+    //    return ans;
     }
 };
